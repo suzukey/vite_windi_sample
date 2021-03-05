3d4d8d6850e66ff30c1d6d169bdae99e42812383
suzukey <suzukei0208@gmail.com>
2021-03-06 02:19:19 +0900

    ✨ React sample

---
+++ react/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="text-center my-8 flex justify-center">
+    <div className="px-8 py-6 rounded-2xl bg-gray-600 text-gray-100">
+      <p>
+        <a
+          rel="noreferrer"
+          href="https://github.com/suzukey/vite_windi_sample"
+          target="_blank"
+        >
+          Vite + React + WindiCSS
+        </a>
+      </p>
+      <p>
+        <em className="text-sm opacity-75">Soooo fast!</em>
+      </p>
+    </div>
+  </div>
+  )
+}
+
+export default App
