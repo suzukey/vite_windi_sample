3682e404e514234b54e7189294e2cd4918e64b32
suzukey <suzukei0208@gmail.com>
2021-03-06 02:19:04 +0900

    ✨ Vue sample

---
+++ vue/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-center my-8 flex justify-center">
+    <div class="px-8 py-6 rounded-2xl bg-gray-600 text-gray-100">
+      <p>
+        <a
+          rel="noreferrer"
+          href="https://github.com/suzukey/vite_windi_sample"
+          target="_blank"
+        >
+          Vite + Vue3 + WindiCSS
+        </a>
+      </p>
+      <p>
+        <em class="text-sm opacity-75">Soooo fast!</em>
+      </p>
+    </div>
+  </div>
+</template>
